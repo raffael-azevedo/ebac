6445955fd7f37058d58b33bbcacfd011fb302351
Raffael Azevedo <raffael.azevedo@gmail.com>
2023-09-21 14:44:18 -0300

    adicionados arquivos de contrato

---
+++ modulo14-teste-api-cypress/cypress/contracts/users.contract.js
@@ -0,0 +1,14 @@
+import Joi from 'joi'
+
+const usersSchema = Joi.object({
+    quantidade: Joi.number(),
+    usuarios: Joi.array().items({
+        administrador: Joi.string(),
+        email: Joi.string(),
+        nome: Joi.string(),
+        password: Joi.string(),
+        _id: Joi.string()
+    })
+})
+
+export default usersSchema
